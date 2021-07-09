e7d3548413cf6ebb965548dad3e153eb89967f06
ShivamGupta-08 <87187728+ShivamGupta-08@users.noreply.github.com>
2021-07-09 19:00:38 +0530

    Create Dictnoary.py

---
+++ Dictnoary.py
@@ -0,0 +1,8 @@
+dic1={"Set":"A collection of distinct entities regarded as a unit.",
+      "Abandoned":"Having been deserted or left.",
+      "Arrogant":"Having or revealing an exaggerated sense of one's own importance or abilities.",
+       "Light": "Give as sensation of vision." }
+print("Enter your word")
+wordin = input()
+wordin.capitalize()
+print( wordin.capitalize(),":",""+dic1[wordin.capitalize()])
